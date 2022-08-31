3cb3950554ac1360d5ed164217b27dfe95261cf8
lucasrapisardi <lucas.rapisardi@live.com>
2022-08-31 17:04:36 -0300

    Testes de tradução de texto-voz

---
+++ src/teste-txt-som.py
@@ -0,0 +1,26 @@
+# Import the required module for text 
+# to speech conversion
+from gtts import gTTS
+  
+# This module is imported so that we can 
+# play the converted audio
+import os
+  
+# The text that you want to convert to audio
+mytext = 'Olá eu sou Lucas!'
+  
+# Language in which you want to convert
+language = 'pt'
+  
+# Passing the text and language to the engine, 
+# here we have marked slow=False. Which tells 
+# the module that the converted audio should 
+# have a high speed
+myobj = gTTS(text=mytext, lang=language, slow=True)
+  
+# Saving the converted audio in a mp3 file named
+# welcome 
+myobj.save("welcome.mp3")
+  
+# Playing the converted file
+os.system("welcome.mp3")
